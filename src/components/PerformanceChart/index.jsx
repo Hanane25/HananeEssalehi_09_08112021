f6c2c042b292b227ebbea33700eed97991fdf7b0
Hanane Essalehi <hanan.essalehi@gmail.com>
2021-11-23 13:39:39 +0100

    fix tichFormatter

--- src/components/PerformanceChart/index.jsx
+++ src/components/PerformanceChart/index.jsx
@@ -72,7 +72,7 @@ class PerformanceChart extends Component {
                             tickLine={false}
                             stroke="white"
                             dataKey="kind"
-                            tickFormatter={(value) => this.state.kind[value][0] + this.state.kind[value].slice(1)}
+                            tickFormatter={(value) => this.state.kind[value]}
                         />
 
                         <Radar
